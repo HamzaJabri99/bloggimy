10e1028e303ff39ae2209d3c6f116f6ba39d5b7b
unknown <jabri.oldd@gmail.com>
2022-10-25 14:23:10 +0100

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,39 @@
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Write from "./pages/Write";
+import SingleBlog from "./pages/SingleBlog";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/path",
+    element: <Write />,
+  },
+  {
+    path: "/blog",
+    element: <SingleBlog />,
+  },
+]);
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
