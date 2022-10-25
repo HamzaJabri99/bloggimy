8d5d68658bbf96f4f948bfc44e00a5927faeb009
unknown <jabri.oldd@gmail.com>
2022-10-25 15:04:42 +0100

    adding outlet/layout for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,45 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Write from "./pages/Write";
 import SingleBlog from "./pages/SingleBlog";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    // path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+      {
+        path: "/post/:id",
+        element: <SingleBlog />,
+      },
+    ],
   },
   {
     path: "/register",
@@ -17,14 +48,6 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />,
-  },
-  {
-    path: "/path",
-    element: <Write />,
-  },
-  {
-    path: "/blog",
-    element: <SingleBlog />,
   },
 ]);
 
