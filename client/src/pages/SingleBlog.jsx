d6b6a7f8eddb28ac66f549bb128bbbd7f38dcd3e
unknown <jabri.oldd@gmail.com>
2022-10-25 22:55:06 +0100

    designing single post page

--- client/src/pages/SingleBlog.jsx
+++ client/src/pages/SingleBlog.jsx
@@ -1,7 +1,65 @@
 import React from "react";
-
+import Menu from "../components/Menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 const SingleBlog = () => {
-  return <div>SingleBlog</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/3131634/pexels-photo-3131634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://avatars.githubusercontent.com/u/75950318?v=4"
+            alt=""
+          />
+          <div className="info">
+            <span>Hamza</span>
+            <p>Posted 3 days ago</p>
+          </div>
+          <div className="edit">
+            <Link className="link" to={"write/?edit=1"}>
+              <FontAwesomeIcon icon={faPenToSquare} />
+            </Link>
+          </div>
+          <div className="delete">
+            <Link className="link">
+              <FontAwesomeIcon icon={faTrashCan} />
+            </Link>
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus
+          excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem
+          ratione sit debitis deserunt repellendus numquam ab vel perspiciatis
+          corporis! Lorem, ipsum dolor sit amet consectetur adipisicing elit. A
+          possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea
+          dolorem ratione sit debitis deserunt repellendus numquam ab vel
+          perspiciatis corporis! Lorem, ipsum dolor sit amet consectetur.
+          <br /> <br />
+          adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam
+          facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus
+          numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. A possimus excepturi aliquid nihil
+          cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis
+          deserunt repellendus numquam ab vel perspiciatis corporis!.
+          <br /> <br />
+          adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam
+          facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus
+          numquam ab vel perspiciatis corporis! Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. A possimus excepturi aliquid nihil
+          cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis
+          deserunt repellendus numquam ab vel perspiciatis corporis!.
+          <br /> <br />
+        </p>
+      </div>
+      <Menu />
+    </div>
+  );
 };
 
 export default SingleBlog;
