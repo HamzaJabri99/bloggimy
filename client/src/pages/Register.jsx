cf81a2de3e9fc2efc45709cf5856b0b4a106a645
unknown <jabri.oldd@gmail.com>
2022-10-26 18:32:46 +0100

    added registering feature in frontend

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,54 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/register", inputs);
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
   return (
     <div className="auth">
       <h1>Create your Account </h1>
-      <form action="">
-        <input type="text" placeholder="username" required />
-        <input type="email" placeholder="email" required />
-        <input type="password" placeholder="password" required />
+      <form action="/api/auth/register" method="post">
+        <input
+          type="text"
+          placeholder="username"
+          required
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          required
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          name="password"
+          onChange={handleChange}
+        />
         <div className="buttonHandler">
-          <button>Register</button> <br />
-          <p>Something went wrong</p>
+          <button onClick={handleSubmit}>Register</button> <br />
+          {error ? <p>{error}</p> : <p></p>}
           <span>
             Do You Have an account? <Link to={"/login"}>Sign in</Link>
           </span>
