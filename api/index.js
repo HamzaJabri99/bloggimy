74c201509ef8a092c6cfbe028c19d610825531a8
unknown <jabri.oldd@gmail.com>
2022-10-26 16:54:41 +0100

    setting up routes and controllers in bakckend

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
+import postRouter from "./routes/posts.js";
+
 const app = express();
 
 //to be able to send data to database
 app.use(express.json());
-app.get("/", (req, res) => {
-  return res.json([{ message: "hello" }]);
-});
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/posts", postRouter);
 app.listen(8800, () => {
   return console.log("Hello from ");
 });
