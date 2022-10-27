9619af8f0db25014baf6f73f350280e41e107985
unknown <jabri.oldd@gmail.com>
2022-10-27 13:57:24 +0100

    creating getPosts functionality

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,18 @@
+import { db } from "../db.js";
+
+export const getPosts = (req, res) => {
+  const q = req.query.cat
+    ? "SELECT*FROM posts WHERE category=?"
+    : "SELECT*FROM posts";
+  db.query(q, [req.query.cat], (error, data) => {
+    if (error) return res(error);
+    return res.status(200).json(data);
+  });
+};
+export const getPost = (req, res) => {};
 export const addPost = (req, res) => {
   res.json("adding the post");
 };
+export const editPost = (req, res) => {};
+
+export const deletePost = (req, res) => {};
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,4 +1,15 @@
 import express from "express";
+import {
+  addPost,
+  deletePost,
+  editPost,
+  getPost,
+  getPosts,
+} from "../controllers/post.js";
 const router = express.Router();
-
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/add", addPost);
+router.put("/:id", editPost);
+router.delete("/:id", deletePost);
 export default router;
